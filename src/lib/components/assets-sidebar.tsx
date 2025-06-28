8b09170e2cc242fc42bb029b70227c1c623fc187
yohanc3 <116668883+yohanc3@users.noreply.github.com>
2025-06-28 18:54:06 -0500

    Updated assets-sidebar.tsx

--- src/lib/components/assets-sidebar.tsx
+++ src/lib/components/assets-sidebar.tsx
@@ -63,7 +63,7 @@ export default function AssetsSidebar({
             </p>
           </div>
         ) : (
-          <div className="space-y-2 mt-2">
+          <div className="space-y-2 mt-[2rem]">
             {assets.map((asset) => (
               <div
                 key={asset.id}
