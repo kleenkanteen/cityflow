4fc9b40fac7ae30d2792197eed013340cff8e673
yohanc3 <116668883+yohanc3@users.noreply.github.com>
2025-06-28 20:44:24 -0500

    Updated db.ts

--- db.ts
+++ db.ts
@@ -1,5 +1,4 @@
 import "dotenv/config";
-import * as schema from "./src/db/schema.ts"
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(process.env.DATABASE_URL!, {schema});
+export const db = drizzle(process.env.DATABASE_URL!);
