9a83bef40b052fd3fa37e062f8031805cb81a88c
yohanc3 <116668883+yohanc3@users.noreply.github.com>
2025-06-28 21:18:35 -0500

    Updated route.ts

--- src/app/api/assets/create/route.ts
+++ src/app/api/assets/create/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { db } from './db';
+import { db } from '@/db';
 import { asset } from '@/src/db/schema';
 
 export async function POST(request: NextRequest) {
